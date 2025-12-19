2fc668bf297771987c9b187aaa772eb940897cfd
Francisco Manuel Suárez <142342067+Paco-S23@users.noreply.github.com>
2025-12-19 15:12:46 -0600

    Update UI.py

--- Stream/UI.py
+++ Stream/UI.py
@@ -218,7 +218,7 @@ st.markdown("""
 <div class="price-box">
   <div class="badge">
     ¡¡ÚLTIMA OPORTUNIDAD!!<br>
-    <span class="badge-sub">🔥ÚLTIMOS 3 DEPARTAMENTOS DISPONIBLES🔥</span>
+    <span class="badge-sub">🔥Últimos 3 Departamentos Disponibles🔥</span>
   </div>
   <div class="price-pill">Desde $2,940,000.00</div>
 </div>
